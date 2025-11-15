dda97f1ea04179c329a93192e4ded98ee40b6e7f
Nguyen Huynh Thi Bich Thao <nhimrung@192.168.2.13>
2025-11-15 20:53:07 +0700

    First commit

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig} from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'https://www.saucedemo.com',
+    setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
+      // you can leave empty if no events are needed
+      return config
+    },
+    specPattern: 'cypress/e2e/**/*.cy.ts',
+  },
+})
